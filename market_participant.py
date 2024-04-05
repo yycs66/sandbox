4527c9a97a3e648651a7f9ca42af3d86096cf68e
liping-li <46494233+yycs66@users.noreply.github.com>
2024-04-05 19:47:45 -0400

    for scaled_offer

--- market_participant.py
+++ market_participant.py
@@ -1,5 +1,5 @@
 '''
-Simulated competitor code for the ESPA-Comp.
+Scaled offer generation
 This code will take the same inputs and provide the same outputs as a competitor algorithm.
 '''
 
@@ -9,6 +9,54 @@ import dummy_algorithm_Br as da
 import json
 import argparse
 import numpy as np
+import random
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+    
+class Scaled_agent():
+    def __init__(self,time_step,market_info,resource_info):
+        self.step = time_step
+        self.market = market_info
+        self.resource = resource_info
+        self.market_type = market_info['market_type']
+        self.rid = resource_info['rid']
+        #self.da = da.Agent(time_step, market_info, resource_info)
+    def scaling(self, da,scaling_factor):
+        '''
+        This function is to generate a scaled_cost_offer based on the dummy_algorithm_Br.py
+        Args:
+            da: the dummy_algorithm_Br object
+            market_type: the market type
+            scaling_factor: the scaling factor
+        Returns:
+            scaled_cost_offer: the scaled cost offer
+        '''
+        # Parse inputs
+
+        dummy_agent = da.Agent(time_step, market_info, resource_info)
+        dummy_offer = dummy_agent.make_me_an_offer()
+        if 'DAM' in self.market_type:
+                keys_factor ={'block_ch_mc':scaling_factor,'block_dc_oc':scaling_factor}
+                for key, factor in keys_factor.items():
+                    dummy_offer[key] = dummy_offer[key] * factor
+                with open(f'offer_{self.step}.json', 'w') as f: #todo: dummy offer's name is needed
+                    json.dump(dummy_offer, f, cls=NpEncoder)
+        else:
+                keys_factor ={'block_soc_mc':scaling_factor,'block_ch_oc':scaling_factor}
+                for key, factor in keys_factor.items():
+                    dummy_offer[key] = dummy_offer[key] * factor
+                with open(f'offer_{self.step}.json', 'w') as f: #todo: dummy offer's name is needed
+                    json.dump(dummy_offer, f, cls=NpEncoder)
+
+        return dummy_offer #updated offer from scaling factor
 
 if __name__ == "__main__":
 
@@ -27,5 +75,11 @@ if __name__ == "__main__":
     with open(args.resource_file, 'r') as f:
         resource_info = json.load(f)
 
-    agent = da.Agent(time_step, market_info, resource_info)
-    agent.make_me_an_offer()
+    factor = random.uniform(0.8, 1.2)
+    scaled_agent = Scaled_agent(time_step,market_info,resource_info)
+    scaled_agent.scaling(da,factor)
+    
+    with open(f'market_{time_step}.json', 'w') as f:
+        json.dump(market_info, f, cls=NpEncoder)
+    with open(f'resource_{time_step}.json', 'w') as f:
+        json.dump(resource_info, f, cls=NpEncoder)  
